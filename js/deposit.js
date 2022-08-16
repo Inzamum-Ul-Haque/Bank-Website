7ffd44c0acc2243d4f992564b7222e5ec2a5c6e7
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-08-17 00:50:08 +0600

    Saving to GirHub

--- js/deposit.js
+++ js/deposit.js
@@ -14,3 +14,11 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
   const newTotalBalance = totalBalance + depositAmount;
   totalBalanceField.innerText = newTotalBalance;
 });
+
+document.getElementById("reset-btn").addEventListener("click", function () {
+  const depositTotal = document.getElementById("deposit-total");
+  const withdrawTotal = document.getElementById("withdraw-total");
+
+  depositTotal.innerText = "00";
+  withdrawTotal.innerText = "00";
+});
