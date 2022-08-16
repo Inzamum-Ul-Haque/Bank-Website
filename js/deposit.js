e1438fb33345dc235e2b6f5cce20547cb74303b6
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-08-17 00:38:19 +0600

    Saving to GitHub

--- js/deposit.js
+++ js/deposit.js
@@ -5,11 +5,11 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
   const depositTotalField = document.getElementById("deposit-total");
   const depositTotal = parseFloat(depositTotalField.innerText);
 
-  depositTotalField.innerText = depositTotal + depositAmount;
-  depositField.value = "";
-
   const totalBalanceField = document.getElementById("total-balance-field");
   const totalBalance = parseFloat(totalBalanceField.innerText);
+
+  depositTotalField.innerText = depositTotal + depositAmount;
+  depositField.value = "";
 
   const newTotalBalance = totalBalance + depositAmount;
   totalBalanceField.innerText = newTotalBalance;
--- js/withdraw.js
+++ js/withdraw.js
@@ -5,11 +5,11 @@ document.getElementById("withdraw-btn").addEventListener("click", function () {
   const withdrawTotalField = document.getElementById("withdraw-total");
   const withdrawTotal = parseFloat(withdrawTotalField.innerText);
 
-  const newWithdrawTotal = withdrawTotal + withdrawAmount;
-  withdrawTotalField.innerText = newWithdrawTotal;
-
   const totalBalanceField = document.getElementById("total-balance-field");
   const totalBalance = parseFloat(totalBalanceField.innerText);
+
+  const newWithdrawTotal = withdrawTotal + withdrawAmount;
+  withdrawTotalField.innerText = newWithdrawTotal;
 
   const newTotalBalance = totalBalance - withdrawAmount;
   totalBalanceField.innerText = newTotalBalance;
